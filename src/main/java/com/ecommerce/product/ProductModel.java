98ba4ee6b35f194222d440dced42016b7f748209
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-06-14 11:56:19 -0300

    TASK-001 feat: adição do Modelo Produto.

---
+++ src/main/java/com/ecommerce/product/ProductModel.java
@@ -0,0 +1,11 @@
+package com.ecommerce.product;
+
+import java.math.BigDecimal;
+
+public class ProductModel {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String color;
+    private String size;
+}
