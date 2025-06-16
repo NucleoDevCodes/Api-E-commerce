724c647cf4e507e9dc29cae9e329921cbcb8ecf4
Otavio <estudosprogotavio@gmail.com>
2025-06-16 09:06:22 -0300

    refactor:alterando o nome do ProductoRepository para ProductRepository

--- src/main/java/com/ecommerce/model/repositorys/ProductRepository.java
+++ src/main/java/com/ecommerce/model/repositorys/ProductRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<ProductModel,Long> {
+public interface ProductRepository extends JpaRepository<ProductModel,Long> {
     boolean existsByNameAndColorAndSize(String name, String color, String size);
 }
