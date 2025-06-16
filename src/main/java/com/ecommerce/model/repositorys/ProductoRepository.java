de2b8872870bdd06e13025f3fbb78df94c913830
Otavio <estudosprogotavio@gmail.com>
2025-06-15 21:18:13 -0300

    feat:adiconando Repositório de Products

---
+++ src/main/java/com/ecommerce/model/repositorys/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.model.repositorys;
+
+import com.ecommerce.model.product.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<ProductModel,Long> {
+}
