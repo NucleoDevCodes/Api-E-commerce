9fc9458f390358db9b8162adbc779d7feef3b80f
Otavio <estudosprogotavio@gmail.com>
2025-06-30 10:08:17 -0300

    TASK005:feat adicionando CategoryType a DataProducts

--- src/main/java/com/ecommerce/aplication/records/DataProducts.java
+++ src/main/java/com/ecommerce/aplication/records/DataProducts.java
@@ -1,6 +1,9 @@
 package com.ecommerce.aplication.records;
 
+import com.ecommerce.model.product.CategoryItem;
+import com.ecommerce.model.product.CategoryType;
+
 import java.math.BigDecimal;
 
-public record DataProducts(String name, BigDecimal price ,String color ,String size) {
+public record DataProducts(String name, BigDecimal price , String color , String size, CategoryItem item) {
 }
