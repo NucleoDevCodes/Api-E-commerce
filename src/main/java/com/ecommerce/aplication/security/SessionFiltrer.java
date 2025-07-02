9882f74473b424a27162c96b9795cd2b4ff7ec02
Otavio <estudosprogotavio@gmail.com>
2025-07-02 15:15:46 -0300

    TASK008:feat adicionando SessionFiltrer

---
+++ src/main/java/com/ecommerce/aplication/security/SessionFiltrer.java
@@ -0,0 +1,41 @@
+package com.ecommerce.aplication.security;
+
+import com.ecommerce.aplication.services.ServiceUsers;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+public class SessionFiltrer extends OncePerRequestFilter  {
+    private final ServiceUsers userService;
+
+    public SessionFiltrer(ServiceUsers userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            var email = (String) request.getSession().getAttribute("USER_EMAIL");
+            if (email != null) {
+                var userDetails = userService.loadUserByUsername(email);
+                var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
