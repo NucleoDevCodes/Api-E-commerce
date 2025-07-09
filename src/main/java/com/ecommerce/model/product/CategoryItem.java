93e2b7d19489686542b7cb5c5f4efc9836f1de00
Otavio <estudosprogotavio@gmail.com>
2025-07-09 13:47:48 -0300

    TASK022: refactor ajustando enum CategoryItem

--- src/main/java/com/ecommerce/model/product/CategoryItem.java
+++ src/main/java/com/ecommerce/model/product/CategoryItem.java
@@ -1,9 +1,33 @@
 package com.ecommerce.model.product;
 
 public enum CategoryItem {
-    CASACOS,
-    VESTIDOS,
-    CAMISAS,
-    CALCADOS,
-    CALÇAS,
+    CAMISETA,
+    CAMISA,
+    BLUSA,
+    CASACO,
+    JAQUETA,
+    MOLETOM,
+    SUÉTER,
+
+    CALÇA,
+    JEANS,
+    BERMUDA,
+    SAIA,
+    LEGGING,
+
+    VESTIDO,
+
+    TÊNIS,
+    BOTA,
+    SANDÁLIA,
+    SAPATO,
+    CHINELO,
+
+    BOLSA,
+    CINTO,
+    BONÉ,
+    CHAPÉU,
+    ÓCULOS,
+    CARTEIRA,
+    RELÓGIO,
 }
