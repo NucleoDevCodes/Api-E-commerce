5a5f6197e34e21ab343d1e1569d9ceac5ff1de36
Otavio <estudosprogotavio@gmail.com>
2025-07-09 13:49:17 -0300

    TASK024: feat adicionando DataProductsResponse

---
+++ src/main/java/com/ecommerce/aplication/records/DataProductsResponse.java
@@ -0,0 +1,17 @@
+package com.ecommerce.aplication.records;
+
+import com.ecommerce.model.product.CategoryItem;
+import com.ecommerce.model.product.CategoryType;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record DataProductsResponse(Long id,
+                                   String name,
+                                   BigDecimal price,
+                                   Integer quant,
+                                   CategoryItem item,
+                                   CategoryType type,
+                                   List<String> sizes,
+                                   List<String> colors) {
+}
