a99cde4d660d1d2afc9bea73c73aa5a307c441cd
Otavio <estudosprogotavio@gmail.com>
2025-07-18 10:10:43 -0300

    TASK-031 feat: adicionando findEmailById em UsersRepositroy

--- src/main/java/com/ecommerce/model/repositorys/UsersRepositroy.java
+++ src/main/java/com/ecommerce/model/repositorys/UsersRepositroy.java
@@ -2,6 +2,7 @@ package com.ecommerce.model.repositorys;
 
 import com.ecommerce.model.users.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface UsersRepositroy extends JpaRepository<Users,Long> {
     Optional<Users> findByEmail(String email);
     Optional<Users> findByIdAndAtivoTrue(Long id);
     Optional<Users> findByEmailAndAtivoTrue(String email);
+    @Query("SELECT u.email FROM Users u WHERE u.id = :id")
+    Optional<String> findEmailById(Long id);
 }
