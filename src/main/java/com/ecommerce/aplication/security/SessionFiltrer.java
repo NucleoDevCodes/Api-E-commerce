1bca1f55c9ea8436454300718290c6be5b2827f9
Otavio <estudosprogotavio@gmail.com>
2025-07-21 09:02:59 -0300

    TASK-033 feat: verifcações para as novas controllers em SessionFiltrer

--- src/main/java/com/ecommerce/aplication/security/SessionFiltrer.java
+++ src/main/java/com/ecommerce/aplication/security/SessionFiltrer.java
@@ -15,7 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 
-public class SessionFiltrer extends OncePerRequestFilter  {
+public class SessionFiltrer extends OncePerRequestFilter {
     private final ServiceUsers userService;
 
     public SessionFiltrer(ServiceUsers userService) {
