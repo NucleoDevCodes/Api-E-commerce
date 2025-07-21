1fcef862102ebc6082a3d2d274fafa380f408abc
Otavio <estudosprogotavio@gmail.com>
2025-07-21 08:59:30 -0300

    TASK-032 feat: adicionando Logger em ServiceAuth

--- src/main/java/com/ecommerce/aplication/services/ServiceAuth.java
+++ src/main/java/com/ecommerce/aplication/services/ServiceAuth.java
@@ -5,6 +5,8 @@ import com.ecommerce.aplication.records.UsersRecords.DataUserLogin;
 import com.ecommerce.aplication.records.UsersRecords.DataUserResponse;
 import com.ecommerce.infra.exceptions.UnauthorizedActionException;
 import com.ecommerce.model.users.Users;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceAuth {
+
+    private final Logger logger = LoggerFactory.getLogger(ServiceAuth.class);
     private final AuthenticationManager authenticationManager;
 
     public ServiceAuth(AuthenticationManager authenticationManager) {
@@ -21,14 +25,20 @@ public class ServiceAuth {
 
     public DataUserResponse login(DataUserLogin data) {
         try {
+            logger.info("Tentativa de login para o e-mail: {}", data.email());
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(data.email(), data.password())
             );
 
             Users user = (Users) authentication.getPrincipal();
-            return new DataUserResponse(user.getId(), user.getUsername(),user.getName(), user.getEmail(),user.getRole());
+            logger.info("Login realizado com sucesso para o usuário ID: {}", user.getId());
+
+            return new DataUserResponse(user.getId(), user.getUsername(), user.getName(), user.getEmail(), user.getRole());
+
         } catch (AuthenticationException ex) {
+            logger.warn("Falha na autenticação para o e-mail: {}", data.email());
             throw new UnauthorizedActionException("Credenciais inválidas.");
         }
+
     }
 }
