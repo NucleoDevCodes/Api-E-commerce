82b5f58db26833359d2d518004000f68a07cca86
Otavio <estudosprogotavio@gmail.com>
2025-07-22 09:17:13 -0300

    TASK-034 feat: adicionando  novos atributos em DataCartItemsResponse

--- src/main/java/com/ecommerce/aplication/records/CartRecords/DataCartItemResponse.java
+++ src/main/java/com/ecommerce/aplication/records/CartRecords/DataCartItemResponse.java
@@ -1,4 +1,4 @@
 package com.ecommerce.aplication.records.CartRecords;
 
-public record DataCartItemResponse(Long productId, String ProductName,Integer quantity) {
+public record DataCartItemResponse(Long productId, String productName, Integer quantity, String color, String size) {
 }
